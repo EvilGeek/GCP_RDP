ca228900971c79535cb5024057694682a286960d
vaibhavchandra2007 <110343574+vaibhavchandra2007@users.noreply.github.com>
2022-09-14 22:10:45 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP, please have patience... " >&2
 {
-sudo useradd -m ANI
-sudo adduser ANI sudo
-echo 'ANI:1234' | sudo chpasswd
+sudo useradd -m WOLF
+sudo adduser WOLF sudo
+echo 'WOLF:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser ANI chrome-remote-desktop
+sudo adduser WOLF chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ANI -c """$CRP"""
+su - WOLF -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
