df8a781c8c9cc41c16239a2058a11adac42935d9
vaibhavchandra2007 <110343574+vaibhavchandra2007@users.noreply.github.com>
2022-09-14 22:19:30 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,34 +1,11 @@
 #! /bin/bash
-printf "Installing RDP, please have patience... " >&2
-{
-sudo useradd -m WOLF
-sudo adduser WOLF sudo
-echo 'WOLF:1234' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get install xfce4 xfce4-terminal
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
+sudo dpkg -i chrome*
+sudo apt-get install -f 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser WOLF chrome-remote-desktop
-} &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - WOLF -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Complete " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
